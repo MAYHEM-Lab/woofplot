e3753b087fa2fb6bd23cb3187ab72c47e1f05694
Chandra Krintz <ckrintz@ucsb.edu>
2026-07-03 14:08:40 -0700

    adding the env variable for reducing decimation

--- RESTART.sh
+++ RESTART.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#uncomment this if you do not want your data decimated for the 60minute viewing window
+#export DECIMATE_SKIP=1500
+
 sudo /etc/init.d/postgresql start   #make sure the database is started
 
 # First check that redis-server is running (ps auxww |grep redis-server), if not start it with this:
